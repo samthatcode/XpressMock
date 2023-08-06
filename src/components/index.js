5efa0ff07db3e2b683d1499c8767f2279bb0b0b8
samthatcode <samthatcode@gmail.com>
2023-08-06 15:35:14 +0100

    Update: Bug fix

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,3 @@ export { default as Navbar } from './Navbar';
 export { default as Home } from './Home';
 export { default as Dashboard } from './Dashboard';
 export { default as Pending } from './Pending';
-export { default as ProtectedRoute } from './ProtectedRoute';
